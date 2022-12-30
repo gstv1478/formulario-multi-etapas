72b3fda1101e5d3e6e0ac69afb5d73458226743f
gstv1478 <gstv1478@gmail.com>
2022-12-29 23:16:55 -0300

    quarto commit

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,7 +1,37 @@
+import {
+    BsFillEmojiHeartEyesFill,
+    BsFillEmojiSmileFill,
+    BsFillEmojiNeutralFill,
+    BsFillEmojiFrownFill
+}   from "react-icons/bs"
+
+
+
 const ReviewForm = () => {
     return(
-        <div>
-            <h2>ReviewForm</h2>
+        <div className="review-form">
+            <div className="form-control score-container">
+                <label className="radio-container">
+                    <input type="radio" value="unsatisfied" name="review" required />
+                    <BsFillEmojiFrownFill />
+                    <p>Insatisfeito</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value="neutral" name="review" required />
+                    <BsFillEmojiNeutralFill/>
+                    <p>Poderia ser melhor</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value="satisfied" name="review" required />
+                    <BsFillEmojiSmileFill />
+                    <p>satisfeito</p>
+                </label>
+                <label className="radio-container">
+                    <input type="radio" value="very_satisfied" name="review" required />
+                    <BsFillEmojiHeartEyesFill />
+                    <p>Muito satisfeito</p>
+                </label>
+            </div>
         </div>
 
     );
